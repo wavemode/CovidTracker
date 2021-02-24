24ad9de5f27895362e8e8898108073ae23db349f
Mohammad Sheikh <gi8310@wayne.edu>
2021-02-24 08:00:56 -0500

    Create CoronavirustrackerApplicationTests.java

---
+++ src/test/java/io/covidtracker/coronavirustracker/CoronavirustrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.covidtracker.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirustrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
