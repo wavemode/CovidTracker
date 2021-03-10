b61d4b8e30b20fbd51902a71461a17bcfbce1d6c
horald-kanani <78053231+horald-kanani@users.noreply.github.com>
2021-03-09 23:10:37 -0500

    Add files via upload
    Create databases and store all the current data

--- CovidTracker/src/main/java/CSC4410/CovidTracker/CovidTrackerApplication.java
+++ CovidTracker/src/main/java/CSC4410/CovidTracker/CovidTrackerApplication.java
@@ -1,5 +1,7 @@
 package CSC4410.CovidTracker;
 
+import CSC4410.CovidTracker.counties.CountyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,8 +11,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class CovidTrackerApplication {
 
+
+@Autowired
+	CountyService countyService = new CountyService();
+
 	public static void main(String[] args) {
 		SpringApplication.run(CovidTrackerApplication.class, args);
+
+
+
 	}
+
+
 
 }
