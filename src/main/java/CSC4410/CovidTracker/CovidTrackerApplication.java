64d2ee9f58f68b3d50dfeba5eb8e181273169b11
horald-kanani <78053231+horald-kanani@users.noreply.github.com>
2021-03-11 00:32:42 -0500

    Made changes in View

---
+++ src/main/java/CSC4410/CovidTracker/CovidTrackerApplication.java
@@ -0,0 +1,23 @@
+package CSC4410.CovidTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+// enable scheduling from the CoronaVirusData
+@EnableScheduling
+
+public class CovidTrackerApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidTrackerApplication.class, args);
+
+
+
+	}
+
+
+}
---
+++ src/main/java/CSC4410/CovidTracker/state/StateRepository.java
@@ -0,0 +1,9 @@
+package CSC4410.CovidTracker.state;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StateRepository extends CrudRepository<StateData,String> {
+
+}
