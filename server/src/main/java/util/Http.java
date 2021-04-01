d24afac3aab4c5a8cfffb330dce07d922addc492
Chukwudi Okechukwu <w4vem0de@gmail.com>
2021-04-01 13:09:08 -0400

    implement Singleton pattern

---
+++ server/src/main/java/util/Http.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.net.http.HttpClient;
+
+public class Http {
+    private static HttpClient client = null;
+
+    private Http() {}
+
+    public static HttpClient getClient() {
+        if (client == null)
+            client = HttpClient.newHttpClient();
+
+        return client;
+    }
+
+}
