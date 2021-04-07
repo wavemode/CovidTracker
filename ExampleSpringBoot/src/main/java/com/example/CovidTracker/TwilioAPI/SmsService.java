b3dffed48699d5022d7f695deab1ff3ea8df4a59
sheikhmo <77809489+sheikhmo@users.noreply.github.com>
2021-04-07 17:19:54 -0400

    Add files via upload

---
+++ ExampleSpringBoot/src/main/java/com/example/CovidTracker/TwilioAPI/SmsService.java
@@ -0,0 +1,23 @@
+package com.example.CovidTracker.TwilioAPI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+
+// service for sms communication
+@org.springframework.stereotype.Service
+public class SmsService {
+
+    private final SenderInterface senderService;
+
+
+    @Autowired
+    // inject the sender interface inside the service
+    public SmsService(@Qualifier("twilio") TwilioSender sender) {
+        this.senderService = sender;
+    }
+// send the requested number
+    public void sendTwilioSms(TwilioNumberRequest numberRequest) {
+        senderService.sendTwilioSms(numberRequest);
+    }
+}
